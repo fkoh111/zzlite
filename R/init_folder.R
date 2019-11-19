0eb01087539a4eb3999724b7d2ac10ec8444f759
Frederik Kok Hansen <frederikkokhansen@pc01184.leo-internal.net>
2019-11-19 18:10:10 +0100

    tests

--- R/init_folder.R
+++ R/init_folder.R
@@ -1,3 +1,15 @@
+# init_folder
+#' Init a a given folder containing .R files
+#' 
+#' This function initializes files with a proper .R extension.
+#' 
+#' @param folder which is a path or a folder name
+#' 
+#' @export
+#' @return nuttin expect for more objects in your environment
+#' 
+#' @examples
+#' init_foldeR("R")
 init_foldeR <- function(folder) {
   src <- file.path(getwd(), folder)
   src_contains <- list.files(path = src, "*.R$")
@@ -6,3 +18,5 @@ init_foldeR <- function(folder) {
     source(file.path(src, file))
   }  
 }
+
+
