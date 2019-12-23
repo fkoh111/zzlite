a4e65555cf256a2c2edaa4c885779e466b8a469f
Frederik Kok Hansen <frederikkokhansen@Frederiks-MBP.lan>
2019-12-23 12:35:47 +0100

    workings scripples for zamzar api

--- R/emf_to_png.R
+++ R/emf_to_png.R
@@ -2,30 +2,27 @@ library(httr)
 library(jsonlite)
 library(dplyr)
 
-### Zamzar API
-
-test_endpoint <- "https://api.zamzar.com/v1/formats/gif"
+test_endpoint <- "https://sandbox.zamzar.com/v1/formats/gif"
 file_path <- file.path(getwd(), "tests", "testthat", "testdata", "avatar.emf", fsep = .Platform$file.sep)
 target <- "png"
-endpoint <- "https://api.zamzar.com/v1/jobs"
-file <- file.path(file_path, fsep = .Platform$file.sep)
-
-keys <- "66b28eb8fa03c69157a68b6926acd02d6ad1406b"
-response_get <- GET(test_endpoint, config = authenticate(key, "")) %>%
-  print()
+#endpoint <- "https://api.zamzar.com/v1/jobs"
+endpoint <- "https://sandbox.zamzar.com/v1/jobs"
 
 
-response_post <- POST(url = endpoint, config = authenticate(key, ""), body = list(
-  source_file = upload_file(file), target_format = "png"
-)) %>%
-  print()
+posted <- POST(url = endpoint,
+                      config = authenticate(fromJSON("inst/key.json")$username, ""),
+                      body = list(source_file = upload_file(file), target_format = "png")
+                      )
 
 
-response_post_get <- GET(endpoint, config = authenticate(key, "")) %>%
-  print()
+status <- GET(endpoint, config = authenticate(fromJSON("inst/key.json")$username, "")) %>%
+  stop_for_status()
 
-stop_for_status(response_post_get)
-got_content <- content(response_post_get, "text")
-as_json <- fromJSON(got_content, flatten = TRUE)
+content <- content(status, as = "text") %>%
+  fromJSON(., flatten = TRUE)
 
-as_json$data$id
+id <- content$data$target_files[[1]][[1]]
+
+url <- paste0("https://sandbox.zamzar.com/v1/files/", id,"/content")
+
+GET("https://sandbox.zamzar.com/v1/files/63569934/content", write_disk("test.png", overwrite = TRUE), config = authenticate(fromJSON("inst/key.json")$username, ""))
