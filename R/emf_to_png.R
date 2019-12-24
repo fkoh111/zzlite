9cb5ef21a373c4f427953fcaf7dfd48616346008
Frederik Kok Hansen <frederikkokhansen@Frederiks-MBP.lan>
2019-12-24 15:23:45 +0100

    license fixes

--- R/emf_to_png.R
+++ R/emf_to_png.R
@@ -2,43 +2,43 @@ library(httr)
 library(jsonlite)
 library(dplyr)
 
-target <- "png"
+#target <- "png"
 
-file <- file.path(getwd(), "tests", "testthat", "testdata", "avatar.emf", fsep = .Platform$file.sep)
-
-
-posted <- POST(url = fromJSON("inst/map.json")$prod$post$endpoint,
-                      config = authenticate(
-                        fromJSON("inst/map.json")$user,
-                        fromJSON("inst/map.json")$pswd
-                      ),
-                      body = list(source_file = upload_file(file),
-                                  target_format = target)
-                    )
+#file <- file.path(getwd(), "tests", "testthat", "testdata", "avatar.emf", fsep = .Platform$file.sep)
 
 
-status <- GET(fromJSON("inst/map.json")$dev$get$endpoint,
-              config = authenticate(
-                fromJSON("inst/map.json")$user,
-                fromJSON("inst/map.json")$pswd
-              )
-            ) %>%
-  stop_for_status()
+#posted <- POST(url = fromJSON("inst/map.json")$prod$post$endpoint,
+#                      config = authenticate(
+#                        fromJSON("inst/map.json")$user,
+#                        fromJSON("inst/map.json")$pswd
+#                      ),
+#                      body = list(source_file = upload_file(file),
+#                                  target_format = target)
+#                    )
 
 
-content <- content(status, as = "text") %>%
-  fromJSON(., flatten = TRUE)
+#status <- GET(fromJSON("inst/map.json")$dev$get$endpoint,
+#              config = authenticate(
+#                fromJSON("inst/map.json")$user,
+#                fromJSON("inst/map.json")$pswd
+#              )
+#            ) %>%
+#  stop_for_status()
 
 
-id <- content$data$id[1]
+#content <- content(status, as = "text") %>%
+#  fromJSON(., flatten = TRUE)
 
 
-url <- paste0(fromJSON("inst/map.json")$dev$get$endpoint, id,"/content")
+#id <- content$data$id[1]
 
-GET(url,
-    write_disk("test.png", overwrite = TRUE),
-    config = authenticate(
-      fromJSON("inst/map.json")$user,
-      fromJSON("inst/map.json")$pswd
-    )
-  )
+
+#url <- paste0(fromJSON("inst/map.json")$dev$get$endpoint, id,"/content")
+
+#GET(url,
+#    write_disk("test.png", overwrite = TRUE),
+#    config = authenticate(
+#      fromJSON("inst/map.json")$user,
+#      fromJSON("inst/map.json")$pswd
+#    )
+#  )
