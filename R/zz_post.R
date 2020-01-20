cfa92efc3f6003262e3e5c099a584e6d98a65be6
fkoh111 <frederik_kok@icloud.com>
2020-01-20 21:42:08 +0100

    Added documentation for zz_post

--- R/zz_post.R
+++ R/zz_post.R
@@ -1,34 +1,43 @@
 # zz_post
 #' 
-#' Simple wrapper for Zamzar endpoint accepting images for conversion
-#' 
-#' 
+#' Post file to Zamzar endpoint
+#'  
 #' Per default zz_post() assumes that you're doing development, thus using a 
 #' development endpoint. Set prod bool to TRUE to change this behaviour.
 #'
-#'
-#' @param file The file you want to convert. Potentially the path to the file
-#' you want to convert.
+#' @param file The path to the file you want to convert.
 #' 
 #' @param extension The file type you want to convert to. E.g., "png".
+#' 
+#' @seealso [zz_format()] for a list of formats you can convert to.
 #' 
 #' @param usr The username/API key you are using for Zamzar.
 #' See: \url{https://developers.zamzar.com/user}
 #' 
-#' @param prod Boolean deciding whether to use a production endpoint or
-#' a development endpoint. Defaults to FALSE (That is, development endpoint).
+#' @param prod Boolean deciding whether to use prod or dev endpoint.
+#' Defaults to FALSE (That is, dev endpoint).
 #'
 #' @export
-#' @return A response object
+#' @return A response object as defined by httr::message_for_status()
 #' 
 #' @examples
 #' \donttest{
-#' zz_post()
+#' 
+#' # Per default zz_post uses the development endpoint
+#' zz_post(file = "avatar.emf", usr = "passwd", extension = "png")
+#' 
+#' # Setting prod param to FALSE is the same as above
+#' zz_post(file = "avatar.emf", usr = "passwd", extension = "png", prod = FALSE)
+#' 
+#' # You need to flip prod to TRUE if you want to use the production endpoint
+#' zz_post(file = "avatar.emf", usr = "passwd", extension = "png", prod = TRUE)
+#' 
 #' }
 
 zz_post <- function(file = NULL, extension = NULL, usr = NULL, prod = FALSE) {
   
-  # Creating temp file if no file has been passed to the file param 
+  # Creating temp file if no file has been passed to the file param
+  #TODO: Consider whether or not this is a good enough approach
   if (is.null(file)) {
     file <- tempfile(fileext = ".tmp")
     writeLines("temp", file)
@@ -62,6 +71,6 @@ zz_post <- function(file = NULL, extension = NULL, usr = NULL, prod = FALSE) {
        ),
        body = body
   )
-  
+
   httr::message_for_status(response)
 }
