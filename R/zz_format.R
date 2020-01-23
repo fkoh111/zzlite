e85e5a3ce3bd3e186a523a8c130937d3a6ce216b
fkoh111 <frederik_kok@icloud.com>
2020-01-23 17:44:53 +0100

    Updates for zz_format

--- R/zz_format.R
+++ R/zz_format.R
@@ -8,21 +8,26 @@
 #' @param usr The username/API key you are using for Zamzar.
 #' See: \url{https://developers.zamzar.com/user}
 #'
-#' @param origin The origin format you want to convert. If param origin is passed,
-#' zz_format returns a list containing a list of targets and a list of costs.
-#' The target list contains tagets that you can convert your origin to.
-#' The cost list contains the cost of for converting between origin and target.
+#' @param origin The origin format you want to convert.
+#' If param origin is passed, zz_format() returns a list of $targets and of $costs.
+#' The target vector contains tagets that you can convert your origin to.
+#' The cost vector contains the cost for conversion between your origin and target.
+#' See: \url{https://developers.zamzar.com/formats}
+#' 
+#' If no orign param has been passed to zz_format(), a character describing
+#' all the origin formats accepted by the Zamzar API is returned.
+#' See: \url{https://developers.zamzar.com/formats}
 #' 
 #' @export
-#' 
 #' @return A list of formats from the API
+#' 
 #' @examples 
 #' \donttest{
 #' zz_format()
 #' 
 #' zz_format(usr = "79b88ef9889d909d533c0099h7432")
 #' 
-#' zz_format(usr = "79b88ef9889d909d533c0099h7432", origin = "exe")
+#' zz_format(usr = "79b88ef9889d909d533c0099h7432", origin = "invalid_origin")
 #' 
 #' zz_format(usr = "79b88ef9889d909d533c0099h7432", origin = "emf")
 #' }
