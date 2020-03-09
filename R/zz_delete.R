ccc0fcab5beb2612dc89f0892a29993ccc72fee5
fkoh111 <frederik_kok@icloud.com>
2020-03-09 19:06:33 +0100

    wrapped in global fun

--- R/zz_delete.R
+++ R/zz_delete.R
@@ -15,5 +15,38 @@ response[['status_code']]
 
 
 zz_delete <- function(id = NULL, usr = NULL, prod = FALSE) {
+ 
+  if (is.null(id)) {
+    stop("Whoops, seems like you forgot to pass an id!")
+  }
   
+  usr <- .zz_get_key(usr = usr)
+  
+  endpoints <- .zz_endpoints()
+  
+  if (prod == FALSE) {
+    endpoint <- endpoints[['dev']][[2]]
+  }
+  
+  if (prod == TRUE) {
+    endpoint <- endpoints[['prod']][[2]]
+  }
+  
+  response <- httr::DELETE(url = paste0(endpoint, id),
+                           config = .zz_authenticate(usr),
+                           .zz_user_agent()
+  )
+   
+  if (!response[['status_code']]  == 200) {
+    stop(sprintf("Zamzar responded with a status code of: %d",
+                 response[['status_code']])
+    )
+  } else {
+    message(response[['status_code']])
+  }
 }
+
+
+zz_get_info()
+
+zz_delete(id = 71047848)
