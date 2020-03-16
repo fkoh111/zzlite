5abb70a4384b9e61baf8e1b2448153a39e436193
fkoh111 <frederik_kok@icloud.com>
2020-03-17 00:57:28 +0100

    documentation for zz_get

--- R/zz_get.R
+++ R/zz_get.R
@@ -1,6 +1,8 @@
 #' Get file from Zamzar account
 #' 
 #' Get file from Zamzar account provided a file id.
+#' Per default `zz_get()` assumes that you're doing development, thus using a 	
+#' development endpoint. Set prod boolean to `TRUE` to change this behavior.
 #'
 #' Please note that a Zamzar key passed as argument to `usr` takes precedence over a
 #' Zamzar key extracted from an `.Renviron`.  
